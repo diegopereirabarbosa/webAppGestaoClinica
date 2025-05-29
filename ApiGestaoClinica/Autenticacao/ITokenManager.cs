437fdce179228460218c0d1c6baac899e4ae41e6
Diego Pereira Barbosa <31224297+diegopereirabarbosa@users.noreply.github.com>
2025-05-29 16:48:23 -0300

    Update ITokenManager.cs

--- ApiGestaoClinica/Autenticacao/ITokenManager.cs
+++ ApiGestaoClinica/Autenticacao/ITokenManager.cs
@@ -1,1 +1,13 @@
+using ApiGestaoClinica.Models;
 
+namespace ApiGestaoClinica.Autenticacao
+{
+    public interface ITokenManager
+    {
+        string GenerateToken(TbSimuladosCadastro tbSimuladosCadastro);
+
+        string GenerateTokenRefreshToken(TbSimuladosCadastro tbSimuladosCadastro);
+
+        Task<(bool isValid, string? nomeLogin, int? idUsuario)> ValidateTokenAsync(string token);
+    }
+}
