3fd1c4dd90b2e37035e9f0a0666420673190e551
Diego Pereira Barbosa <31224297+diegopereirabarbosa@users.noreply.github.com>
2025-05-29 16:49:32 -0300

    Update TokenHelpers.cs

--- ApiGestaoClinica/Autenticacao/TokenHelpers.cs
+++ ApiGestaoClinica/Autenticacao/TokenHelpers.cs
@@ -1,1 +1,25 @@
 
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace ApiGestaoClinica.Autenticacao
+{
+    public interface TokenHelpers
+    {
+        public static TokenValidationParameters GetTokenValidationParameters(IConfiguration configuration)
+        {
+            var tokenKey = Encoding.UTF8.GetBytes(configuration["JwtSettings:SecretKey"] ?? string.Empty);
+            return new TokenValidationParameters()
+            {
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                ValidateAudience = true,
+                ValidAudience = configuration["JwtSettings:Audience"],
+                ValidateIssuer = true,
+                ValidIssuer = configuration["JwtSettings:Issuer"],
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(tokenKey)
+            };
+        }
+    }
+}
